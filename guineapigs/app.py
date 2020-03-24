f9624474d02567378628c176f7d0d6c393509dd5
Mohamad Safadieh <me@msafadieh.com>
2020-03-24 17:14:19 -0400

    fix timezone problems

--- guineapigs/app.py
+++ guineapigs/app.py
@@ -1,53 +1,38 @@
 from datetime import datetime
 import os
 from flask import Flask, render_template, redirect, request
-from pytz import timezone
 from guineapigs.database import Database
 
-TIMEZONE = timezone(os.environ["TIMEZONE"])
 TITLE = os.environ["TITLE"]
 
 def init_flask():
     global app, database
     app = Flask(__name__)
-    database = Database(os.environ["MONGODB_URL"])
+    database = Database()
 
 init_flask()
 
 @app.route("/")
 def index():
-    foods = database.get_foods(datetime.now(TIMEZONE).replace(hour=0, minute=0, second=0, microsecond=0))
     return render_template(
         "index.html",
-        foods=foods,
+        foods=database.get_foods(),
         title=TITLE
         )
 
 @app.route("/submit", methods=["POST"])
 def submit():
-    form = request.form
-    food = {
-        "name": form["name"],
-        "quantity": int(form["quantity"]),
-        "unit": form["unit"],
-        "time": datetime.now(TIMEZONE)
-    }
-    database.add_food(food)
+    database.add_food(request.form["name"],
+                      request.int(form["quantity"]),
+                      request.form["unit"])
     return redirect("/")
 
 @app.route("/vitaminc")
 def vitaminc():
-    food = {
-        "name": "Vitamin C",
-        "quantity": 1,
-        "unit": "pcs",
-        "time": datetime.now(TIMEZONE)
-    }
-    database.add_food(food)
+    database.add_food("Vitamin C", 1, "pcs")
     return redirect("/")
 
 @app.route("/delete", methods=["POST"])
 def delete():
-    _id = request.form["id"]
-    database.delete_food(_id)
+    database.delete_food(request.form["id"])
     return redirect("/")
--- guineapigs/database.py
+++ guineapigs/database.py
@@ -1,18 +1,39 @@
+from datetime import datetime
+import os
+from bson import CodecOptions
 from bson.objectid import ObjectId
 from pymongo import MongoClient
+from pytz import timezone
 
 class Database:
 
-    def __init__(self, host):
-        self.client = MongoClient(host)
+    def __init__(self):
+        self.client = MongoClient(os.environ["MONGODB_URL"])
         self.db = self.client["guineapigs"]    
         self.foods = self.db["foods"]
+        self.timezone = timezone(os.environ["TIMEZONE"])
+        self.timezone_aware = self.foods.with_options(
+            codec_options=CodecOptions(
+                tz_aware=True,
+                tzinfo=self.timezone
+            )
+        )
 
-    def add_food(self, food):
+    def add_food(self, name, quantity, unit):
+        food = {
+            "name": name,
+            "quantity": quantity,
+            "unit": unit,
+            "time": datetime.utcnow()
+        }
         self.foods.insert_one(food)
 
-    def get_foods(self, time):
-        return self.foods.find({"time": {"$gt": time}})
+    def get_foods(self):
+        time = datetime.now(self.timezone).replace(hour=0,
+                                                   minute=0,
+                                                   second=0,
+                                                   microsecond=0)
+        return self.timezone_aware.find({"time": {"$gt": time}})
 
     def delete_food(self, _id):
         self.foods.delete_one({"_id": ObjectId(_id)})
